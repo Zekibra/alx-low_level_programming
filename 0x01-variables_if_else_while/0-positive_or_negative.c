86ab3314956f3c789f8973a67727b4d7c636aeac
Zekibra <111282993+Zekibra@users.noreply.github.com>
2022-09-09 23:14:10 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,7 @@
 int main(void)
 {
 	int n; 
-
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -25,6 +24,5 @@ int main(void)
 		  printf("%d is negative/n", n); 
 	  }
 	   
-
 	return (0);
 }
