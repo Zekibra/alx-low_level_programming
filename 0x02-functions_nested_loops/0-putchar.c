15311327c7ca6ff87c93a1160fc8d36d0f4939a1
Zekibra <abdrebeshir611@gmail.com>
2022-09-13 15:19:18 +0300

    nestes loops v.5

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+char c[] = "_putchar";
+int i = 0;
+for (i = 0; i < 8; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
