4c6b441b214569f1a088332df92b27f39e556875
Zekibra <111282993+Zekibra@users.noreply.github.com>
2022-09-23 06:52:09 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,11 +8,11 @@
 	*/
         void reverse_array(int *a, int n);
 	void print_number(int n);
-	void print buffer(char *b, int size);
+	void print_buffer(char *b, int size);
 	int _strcmp(char *s1, char *s2);
-	char *_strcat(char *dest, char **src);
-	char *_strncat(char *dest, char **src, int n);
-	char *_strncpy(char *dest, char **src, int n);
+	char *_strcat(char *dest, char *src);
+	char *_strncat(char *dest, char *src, int n);
+	char *_strncpy(char *dest, char *src, int n);
 	char *string_toupper(char *);
 	char *cap_string(char *)
 	char *leet(char *);
